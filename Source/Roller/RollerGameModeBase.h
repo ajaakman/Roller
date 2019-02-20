90e58f9e209f6a3d8a05d12c1b855f5d1175fc68
Artur Jaakman <jaakmanartur@gmail.com>
2019-02-20 22:44:29 +0200

    Initial commit

---
+++ Source/Roller/RollerGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RollerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLLER_API ARollerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/RollerEditor.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class RollerEditorTarget : TargetRules
+{
+	public RollerEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+
+		ExtraModuleNames.AddRange( new string[] { "Roller" } );
+	}
+}
