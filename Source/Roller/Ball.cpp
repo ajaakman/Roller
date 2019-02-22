55467480ef3971b7b972dd5d9c2ccaadb557f42d
Artur Jaakman <jaakmanartur@gmail.com>
2019-02-22 21:44:08 +0200

    Added Ball Pawn

---
+++ Source/Roller/Ball.cpp
@@ -0,0 +1,130 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Ball.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Engine/CollisionProfile.h"
+#include "Engine/StaticMesh.h"
+#include "EngineUtils.h"
+
+// Sets default values
+ABall::ABall()
+{
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BallMesh(TEXT("/Game/Meshes/BallMesh.BallMesh"));
+
+	// Create mesh component for the ball
+	Ball = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ball0"));
+	Ball->SetStaticMesh(BallMesh.Object);
+	Ball->BodyInstance.SetCollisionProfileName(UCollisionProfile::PhysicsActor_ProfileName);
+	Ball->SetSimulatePhysics(true);
+	Ball->SetAngularDamping(0.1f);
+	Ball->SetLinearDamping(0.1f);
+	Ball->BodyInstance.MassScale = 3.5f;
+	Ball->BodyInstance.MaxAngularVelocity = 800.0f;
+	Ball->SetNotifyRigidBodyCollision(true);
+	RootComponent = Ball;
+
+	// Create a camera boom attached to the root (ball)
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm0"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bAbsoluteRotation = true; // Rotation of the ball should not affect rotation of boom
+	SpringArm->RelativeRotation = FRotator(-45.f, 0.f, 0.f);
+	SpringArm->TargetArmLength = 1200.f;
+	SpringArm->bEnableCameraLag = false;
+	SpringArm->CameraLagSpeed = 3.f;
+
+	// Create a camera and attach to boom
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera0"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false; // We don't want the controller rotating the camera
+
+	// Set up forces
+	RollTorque = 50000000.0f;
+	JumpImpulse = 350000.0f;
+	bCanJump = true; // Start being able to jump
+}
+
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+	//RewindInit(this->Ball, this);
+}
+
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//MoveForward(1.0f);
+	//if (!s_ui8RemainingRewind) RewindEnd();
+	//RewindTick(DeltaTime, this->Ball, this);
+}
+
+// Called to bind functionality to input
+void ABall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// set up gameplay key bindings
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABall::MoveRight);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABall::MoveForward);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABall::Jump);
+	//PlayerInputComponent->BindAction("Rewind", IE_Pressed, this, &ABall::RewindStart);
+	//PlayerInputComponent->BindAction("Rewind", IE_Released, this, &ABall::RewindEnd);
+
+	// handle touch devices
+	PlayerInputComponent->BindTouch(IE_Pressed, this, &ABall::TouchStarted);
+	PlayerInputComponent->BindTouch(IE_Released, this, &ABall::TouchStopped);
+}
+
+void ABall::MoveRight(float Val)
+{
+	const FVector Torque = FVector(-1.f * Val * RollTorque, 0.f, 0.f);
+	Ball->AddTorqueInRadians(Torque);
+}
+
+void ABall::MoveForward(float Val)
+{
+	const FVector Torque = FVector(0.f, Val * RollTorque, 0.f);
+	Ball->AddTorqueInRadians(Torque);
+}
+
+void ABall::Jump()
+{
+	if (bCanJump)
+	{
+		const FVector Impulse = FVector(0.f, 0.f, JumpImpulse);
+		Ball->AddImpulse(Impulse);
+		bCanJump = false;
+	}
+}
+
+void ABall::NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+
+	bCanJump = true;
+}
+
+void ABall::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
+{
+	if (bCanJump)
+	{
+		const FVector Impulse = FVector(0.f, 0.f, JumpImpulse);
+		Ball->AddImpulse(Impulse);
+		bCanJump = false;
+	}
+
+}
+
+void ABall::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
+{
+	if (bCanJump)
+	{
+		const FVector Impulse = FVector(0.f, 0.f, JumpImpulse);
+		Ball->AddImpulse(Impulse);
+		bCanJump = false;
+	}
+}
+
